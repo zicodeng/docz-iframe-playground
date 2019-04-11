a9442d973bbfe5a646b622149f608c4f50495eda
Zico Deng <zicodeng@gmail.com>
2019-04-11 14:25:37 -0700

    Deploy docs

--- docs/static/js/4.dedcc1c4.js
+++ docs/static/js/4.dedcc1c4.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"./.docz/app/imports.js":function(n,t,o){"use strict";o.r(t),o.d(t,"imports",function(){return e});var e={"test/MyComponent.mdx":function(){return o.e(2).then(o.bind(null,"./test/MyComponent.mdx"))}}}},0,[2]]);
-//# sourceMappingURL=4.349cefa33305f69ee8c4.js.map
+//# sourceMappingURL=4.045026f6f1deaba66843.js.map
