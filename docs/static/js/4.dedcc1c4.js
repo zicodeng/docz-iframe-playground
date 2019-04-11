c591dc759c9209c23c946a1b9f6947fc1c6a7fca
Zico Deng <zicodeng@gmail.com>
2019-04-11 14:39:20 -0700

    Redeploy demo

--- docs/static/js/4.dedcc1c4.js
+++ docs/static/js/4.dedcc1c4.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"./.docz/app/imports.js":function(n,t,o){"use strict";o.r(t),o.d(t,"imports",function(){return e});var e={"test/MyComponent.mdx":function(){return o.e(2).then(o.bind(null,"./test/MyComponent.mdx"))}}}},0,[2]]);
-//# sourceMappingURL=4.31c44033104109a2f7d0.js.map
+//# sourceMappingURL=4.99bc610dee093d84e5f4.js.map
