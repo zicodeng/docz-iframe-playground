2bde8b07279b416689717f2304763c8f4329c8bf
Zico Deng <zicodeng@gmail.com>
2019-04-11 17:31:31 -0700

    Redeploy docs

--- docs/static/js/4.dedcc1c4.js
+++ docs/static/js/4.dedcc1c4.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"./.docz/app/imports.js":function(n,t,o){"use strict";o.r(t),o.d(t,"imports",function(){return e});var e={"test/MyComponent.mdx":function(){return o.e(2).then(o.bind(null,"./test/MyComponent.mdx"))}}}},0,[2]]);
-//# sourceMappingURL=4.99bc610dee093d84e5f4.js.map
+//# sourceMappingURL=4.6d18048570bfccd4f4e1.js.map
