fbf0bcfa1f5cd01102007b5c84baccdb8bcb5078
Zico Deng <zicodeng@gmail.com>
2019-04-13 01:24:03 -0700

    Deploy demo

--- docs/static/js/4.6940e840.js
+++ docs/static/js/4.6940e840.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"./.docz/app/imports.js":function(n,r,t){"use strict";t.r(r),t.d(r,"imports",function(){return e});var e={"test/IFramePlayground.mdx":function(){return Promise.all([t.e(0),t.e(3)]).then(t.bind(null,"./test/IFramePlayground.mdx"))}}}},0,[0,3]]);
-//# sourceMappingURL=4.9b8a578e835312f89fa4.js.map
+//# sourceMappingURL=4.f773cd2244b81654e8d5.js.map
